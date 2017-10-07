27b55c596c039602bc6a4cf1d5216ea5b3bee8c7
AJ Keller <pushtheworldllc@gmail.com>
2017-10-07 17:15:45 -0600

    WIP: LSL example

--- examples/labstreaminglayer/index.js
+++ examples/labstreaminglayer/index.js
@@ -37,26 +37,25 @@ ourBoard.autoFindOpenBCIBoard().then(portName => {
     // Call to connect
     ourBoard.connect(portName)
       .then(() => {
-        ourBoard.on('ready', () => {
-          // Get the sample rate after 'ready'
-          numChans = ourBoard.numberOfChannels();
-          if (numChans === 16) {
-            ourBoard.overrideInfoForBoardType('daisy');
-          }
 
-          // Find out if you can even time sync, you must be using v2 and this is only accurate after a `.softReset()` call which is called internally on `.connect()`. We parse the `.softReset()` response for the presence of firmware version 2 properties.
-          timeSyncPossible = ourBoard.usingAtLeastVersionTwoFirmware();
+        // Get the sample rate after 'ready'
+        numChans = ourBoard.numberOfChannels();
+        if (numChans === 16) {
+          ourBoard.overrideInfoForBoardType('daisy');
+        }
 
-          sendToPython({'numChans': numChans, 'sampleRate': ourBoard.sampleRate()});
-          if (timeSyncPossible) {
-            ourBoard.streamStart()
-              .catch(err => {
-                console.log(`stream start: ${err}`);
-              });
-          } else {
-            console.log('not able to time sync');
-          }
-        });
+        // Find out if you can even time sync, you must be using v2 and this is only accurate after a `.softReset()` call which is called internally on `.connect()`. We parse the `.softReset()` response for the presence of firmware version 2 properties.
+        timeSyncPossible = ourBoard.usingAtLeastVersionTwoFirmware();
+
+        sendToPython({'numChans': numChans, 'sampleRate': ourBoard.sampleRate()}, verbose);
+        if (timeSyncPossible) {
+          ourBoard.streamStart()
+            .catch(err => {
+              console.log(`stream start: ${err}`);
+            });
+        } else {
+          console.log('not able to time sync');
+        }
       })
       .catch(err => {
         console.log(`connect: ${err}`);
@@ -90,7 +89,7 @@ const sampleFunc = sample => {
         action: 'process',
         command: 'sample',
         message: sample
-      });
+      }, verbose);
     }
   }
 };
