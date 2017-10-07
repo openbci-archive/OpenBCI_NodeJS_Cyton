b1d9bed3b498d395394b698d1154674d32e4a556
AJ Keller <pushtheworldllc@gmail.com>
2017-10-07 17:29:15 -0600

    FIX: Add lsl

--- examples/labstreaminglayer/index.js
+++ examples/labstreaminglayer/index.js
@@ -44,7 +44,7 @@ ourBoard.autoFindOpenBCIBoard().then(portName => {
           }
 
           // Find out if you can even time sync, you must be using v2 and this is only accurate after a `.softReset()` call which is called internally on `.connect()`. We parse the `.softReset()` response for the presence of firmware version 2 properties.
-          timeSyncPossible = ourBoard.usingVersionTwoFirmware();
+          timeSyncPossible = ourBoard.usingAtLeastVersionTwoFirmware();
 
           sendToPython({'numChans': numChans, 'sampleRate': ourBoard.sampleRate()});
           if (timeSyncPossible) {
