282474f5548b447dbc90f917c311a77a333b8ed2
AJ Keller <aj@neurosity.co>
2019-02-11 11:20:30 -0500

    FIX: examples

--- examples/debug/debug.js
+++ examples/debug/debug.js
@@ -13,7 +13,8 @@
 const stream = true;
 const debug = true; // Pretty print any bytes in and out... it's amazing...
 const verbose = true; // Adds verbosity to functions
-const Cyton = require('openbci').Cyton;
+const Cyton = require("../../openBCICyton");
+
 let ourBoard = new Cyton({
   debug: debug,
   verbose: verbose
@@ -27,15 +28,17 @@ ourBoard.autoFindOpenBCIBoard().then(portName => {
      * i.e. ourBoard.connect(portName).....
      */
     // Call to connect
-    ourBoard.connect(portName).then(() => {
-      console.log(`connected`);
-    })
+    ourBoard
+      .connect(portName)
+      .then(() => {
+        console.log(`connected`);
+      })
       .catch(err => {
         console.log(`connect: ${err}`);
       });
   } else {
     /** Unable to auto find OpenBCI board */
-    console.log('Unable to auto find OpenBCI board');
+    console.log("Unable to auto find OpenBCI board");
   }
 });
 
@@ -45,10 +48,9 @@ ourBoard.autoFindOpenBCIBoard().then(portName => {
 var readyFunc = () => {
   // Get the sample rate after 'ready'
   if (stream) {
-    ourBoard.streamStart()
-      .catch(err => {
-        console.log(`stream start: ${err}`);
-      });
+    ourBoard.streamStart().catch(err => {
+      console.log(`stream start: ${err}`);
+    });
   }
 };
 
@@ -60,18 +62,18 @@ var sampleFunc = sample => {
 };
 
 // Subscribe to your functions
-ourBoard.on('ready', readyFunc);
-ourBoard.on('sample', sampleFunc);
+ourBoard.on("ready", readyFunc);
+ourBoard.on("sample", sampleFunc);
 
-function exitHandler (options, err) {
+function exitHandler(options, err) {
   if (options.cleanup) {
-    if (verbose) console.log('clean');
+    if (verbose) console.log("clean");
     ourBoard.removeAllListeners();
     /** Do additional clean up here */
   }
   if (err) console.log(err.stack);
   if (options.exit) {
-    if (verbose) console.log('exit');
+    if (verbose) console.log("exit");
     if (stream) {
       ourBoard.streamStop().catch(console.log);
     }
@@ -79,28 +81,37 @@ function exitHandler (options, err) {
   }
 }
 
-if (process.platform === 'win32') {
-  const rl = require('readline').createInterface({
+if (process.platform === "win32") {
+  const rl = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
   });
 
-  rl.on('SIGINT', function () {
-    process.emit('SIGINT');
+  rl.on("SIGINT", function() {
+    process.emit("SIGINT");
   });
 }
 
 // do something when app is closing
-process.on('exit', exitHandler.bind(null, {
-  cleanup: true
-}));
+process.on(
+  "exit",
+  exitHandler.bind(null, {
+    cleanup: true
+  })
+);
 
 // catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {
-  exit: true
-}));
+process.on(
+  "SIGINT",
+  exitHandler.bind(null, {
+    exit: true
+  })
+);
 
 // catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {
-  exit: true
-}));
+process.on(
+  "uncaughtException",
+  exitHandler.bind(null, {
+    exit: true
+  })
+);
--- examples/timeSync/timeSync.js
+++ examples/timeSync/timeSync.js
@@ -1,16 +1,16 @@
 /**
-* This is an example of time syncing every second for one minute. Used with a
-*  real board.
-* To run:
-*   change directory to this file `cd examples/timeSync`
-*   do `npm install`
-*   then `npm start`
-*/
+ * This is an example of time syncing every second for one minute. Used with a
+ *  real board.
+ * To run:
+ *   change directory to this file `cd examples/timeSync`
+ *   do `npm install`
+ *   then `npm start`
+ */
 const verbose = true; // Adds verbosity to functions
 
-const Cyton = require('openbci').Cyton;
+const Cyton = require("@openbci/cyton").Cyton;
 let ourBoard = new Cyton({
-  simulatorFirmwareVersion: 'v2',
+  simulatorFirmwareVersion: "v2",
   verbose: verbose
 });
 
@@ -20,20 +20,22 @@ let timeSyncPossible = false;
 ourBoard.autoFindOpenBCIBoard().then(portName => {
   if (portName) {
     /**
-    * Connect to the board with portName
-    * i.e. ourBoard.connect(portName).....
-    */
+     * Connect to the board with portName
+     * i.e. ourBoard.connect(portName).....
+     */
     // Call to connect
-    ourBoard.connect(portName).then(() => {
-      console.log(`connected`);
-      readyFunc();
-    })
+    ourBoard
+      .connect(portName)
+      .then(() => {
+        console.log(`connected`);
+        readyFunc();
+      })
       .catch(err => {
         console.log(`connect: ${err}`);
       });
   } else {
     /** Unable to auto find OpenBCI board */
-    console.log('Unable to auto find OpenBCI board');
+    console.log("Unable to auto find OpenBCI board");
   }
 });
 
@@ -44,40 +46,39 @@ const readyFunc = () => {
   timeSyncPossible = ourBoard.usingVersionTwoFirmware();
 
   if (timeSyncPossible) {
-    ourBoard.streamStart()
-      .catch(err => {
-        console.log(`stream start: ${err}`);
-      });
+    ourBoard.streamStart().catch(err => {
+      console.log(`stream start: ${err}`);
+    });
   } else {
     killFunc();
   }
 };
 
 const killFunc = () => {
-  ourBoard.disconnect()
-    .then(() => {
-      process.kill();
-    });
+  ourBoard.disconnect().then(() => {
+    process.kill();
+  });
 };
 
 const sampleFunc = sample => {
   // Resynchronize every every second
   if (sample._count % (sampleRate * 1) === 0) {
-    ourBoard.syncClocksFull()
-      .then(syncObj => {
-        // Sync was successful
-        if (syncObj.valid) {
-          // Log the object to check it out!
-          console.log(`timeOffset`, syncObj.timeOffsetMaster);
-        } else {
-          // Retry it
-          console.log(`Was not able to sync... retry!`);
-        }
-      });
+    ourBoard.syncClocksFull().then(syncObj => {
+      // Sync was successful
+      if (syncObj.valid) {
+        // Log the object to check it out!
+        console.log(`timeOffset`, syncObj.timeOffsetMaster);
+      } else {
+        // Retry it
+        console.log(`Was not able to sync... retry!`);
+      }
+    });
   }
 
-  if (sample.timeStamp) { // true after the first successful sync
-    if (sample.timeStamp < 10 * 60 * 60 * 1000) { // Less than 10 hours
+  if (sample.timeStamp) {
+    // true after the first successful sync
+    if (sample.timeStamp < 10 * 60 * 60 * 1000) {
+      // Less than 10 hours
       console.log(`Bad time sync ${sample.timeStamp}`);
     }
   }
@@ -90,42 +91,51 @@ const sampleFunc = sample => {
 
 // Subscribe to your functions
 // ourBoard.on('ready', readyFunc);
-ourBoard.on('sample', sampleFunc);
+ourBoard.on("sample", sampleFunc);
 
-function exitHandler (options, err) {
+function exitHandler(options, err) {
   if (options.cleanup) {
-    if (verbose) console.log('clean');
+    if (verbose) console.log("clean");
     /** Do additional clean up here */
   }
   if (err) console.log(err.stack);
   if (options.exit) {
-    if (verbose) console.log('exit');
+    if (verbose) console.log("exit");
     ourBoard.disconnect().catch(console.log);
   }
 }
 
-if (process.platform === 'win32') {
-  const rl = require('readline').createInterface({
+if (process.platform === "win32") {
+  const rl = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
   });
 
-  rl.on('SIGINT', function () {
-    process.emit('SIGINT');
+  rl.on("SIGINT", function() {
+    process.emit("SIGINT");
   });
 }
 
 // do something when app is closing
-process.on('exit', exitHandler.bind(null, {
-  cleanup: true
-}));
+process.on(
+  "exit",
+  exitHandler.bind(null, {
+    cleanup: true
+  })
+);
 
 // catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {
-  exit: true
-}));
+process.on(
+  "SIGINT",
+  exitHandler.bind(null, {
+    exit: true
+  })
+);
 
 // catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {
-  exit: true
-}));
+process.on(
+  "uncaughtException",
+  exitHandler.bind(null, {
+    exit: true
+  })
+);
